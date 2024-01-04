59db50a71dac7bc85d95a26d16615e514e176a63
Prashant Jain <prashantjain0501@gmail.com>
2024-01-04 10:12:17 +0530

    First commit from master

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class HelloWorld {
+	public static void main(String args[]) {
+		System.out.println("Hello World from the master branch");
+		System.exit(0);
+	}
+}
